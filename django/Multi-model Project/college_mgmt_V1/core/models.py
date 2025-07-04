5bc8080a510b0d0c18b597f3716c708ba660c83b
akshay <akshayarao.parvam@gmail.com>
2025-07-04 12:05:11 +0530

    Full Stack Project - College Management Version 1

---
+++ django/Multi-model Project/college_mgmt_V1/core/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    branch = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date_enrolled = models.DateField(auto_now_add=True)
