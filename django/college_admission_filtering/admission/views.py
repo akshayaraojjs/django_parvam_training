90b55c0d0cb74d959a9a934fa1e934e0b0713426
akshayaraojjs <akshayarao.parvam@gmail.com>
2025-07-16 17:00:34 +0530

    Fixed the Filtering Issue for Start & End Date

--- django/college_admission_filtering/admission/views.py
+++ django/college_admission_filtering/admission/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render
 from .models import Student
+from datetime import datetime
 
 def student_list(request):
     students = Student.objects.select_related('branch', 'admission')
 
-    gender = request.GET.get('gender')
+    gender_filter = request.GET.get('gender')
     branch = request.GET.get('branch')
     sort_by = request.GET.get('sort')
 
-    if gender:
-        students = students.filter(gender=gender)
+    if gender_filter:
+        students = students.filter(gender=gender_filter)
 
     if branch:
         students = students.filter(branch__name=branch)
 
-    if 'start' in request.GET and 'end' in request.GET:
-        start_date = request.GET.get('start')
-        end_date = request.GET.get('end')
-        students = students.filter(registration_date__range=[start_date, end_date])
+    # if 'start' in request.GET and 'end' in request.GET:
+    #     start_date = request.GET.get('start')
+    #     end_date = request.GET.get('end')
+    #     students = students.filter(registration_date__range=[start_date, end_date])
+
+    start_date_str = request.GET.get('start')
+    end_date_str = request.GET.get('end')
+    
+    if start_date_str and end_date_str:
+        try:
+            start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
+            end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
+            students = students.filter(registration_date__range=[start_date, end_date])
+        except ValueError:
+            pass
+            # print("Invalid date format. Please use YYYY-MM-DD.")
 
     if sort_by == 'fees':
         students = students.order_by('admission__fees')
